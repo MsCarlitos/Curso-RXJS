41209c2bd6ad927efa381ef792ec9d669f4e05d9
MsCarlitos <camendozacontreras5@gmail.com>
2023-06-21 13:25:08 -0700

    ejercicios

---
+++ src/ejercicios-terminados/04-cuenta-regresiva.ts
@@ -0,0 +1,32 @@
+import { interval, map, range, take } from 'rxjs';
+
+/**
+ * Ejercicio: Realizar una cuenta regresiva
+ * empezando de 7
+ */
+
+// Salida esperada ===
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+
+(() => {
+
+    const inicio = 8;
+    const countdown$ = interval(700).pipe(
+        // Usar los operadores necesarios
+        // para realizar la cuenta regresiva
+        map( i=> inicio -i ),
+        take(inicio + 1)
+    );
+    // No tocar esta línea ==================
+    countdown$.subscribe(console.log); // =
+    // ======================================
+
+
+})();
